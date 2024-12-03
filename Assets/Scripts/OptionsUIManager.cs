24db6333961c78626b17cdb94b0bb05c773f2bd5
Cameron Traer <crtraer@gmail.com>
2024-12-03 02:17:41 -0400

    Added a State System For Options
    Code provided by ChatGPT, used to replace the need for multiple timelines to and from each button.

--- Assets/Scripts/OptionsUIManager.cs
+++ Assets/Scripts/OptionsUIManager.cs
@@ -1,18 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class OptionsUIManager : MonoBehaviour
+public class OptionsMenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // References to each submenu GameObject
+    public GameObject gameplayMenu;
+    public GameObject audioMenu;
+    public GameObject videoMenu;
+    public GameObject accessibilityMenu;
+
+    // Start by hiding all submenus
     void Start()
     {
-        
+        HideAllMenus();
     }
 
-    // Update is called once per frame
-    void Update()
+    // Show the Gameplay submenu
+    public void ShowGameplayMenu()
     {
-        
+        HideAllMenus();
+        gameplayMenu.SetActive(true);
+    }
+
+    // Show the Audio submenu
+    public void ShowAudioMenu()
+    {
+        HideAllMenus();
+        audioMenu.SetActive(true);
+    }
+
+    // Show the Video submenu
+    public void ShowVideoMenu()
+    {
+        HideAllMenus();
+        videoMenu.SetActive(true);
+    }
+
+    // Show the Accessibility submenu
+    public void ShowAccessibilityMenu()
+    {
+        HideAllMenus();
+        accessibilityMenu.SetActive(true);
+    }
+
+    // Hide all submenus (for the Main Menu button)
+    public void HideAllMenus()
+    {
+        // This will hide all submenus
+        gameplayMenu.SetActive(false);
+        audioMenu.SetActive(false);
+        videoMenu.SetActive(false);
+        accessibilityMenu.SetActive(false);
     }
 }
